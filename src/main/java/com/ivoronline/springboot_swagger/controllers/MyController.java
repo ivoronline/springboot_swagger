38c02f5ab45ed0c36810c1955751b259ddbe1e4e
igoreta <ivoronline@gmail.com>
2022-03-07 11:13:42 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_swagger/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_swagger/controllers/MyController.java
@@ -1,17 +1,50 @@
 package com.ivoronline.springboot_swagger.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Controller
+@RestController
+@EnableSwagger2
 public class MyController {
 
-  @ResponseBody
-  @RequestMapping("/Hello")
-  public String hello(@RequestParam String name, @RequestParam Integer age) {
+  //=========================================================================
+  // ONE METHOD
+  //=========================================================================
+  @RequestMapping("Hello")
+    String hello(
+      @RequestParam String  name,
+      @RequestParam Integer age
+  ) {
     return name + " is " + age + " years old";
   }
 
+  //=========================================================================
+  // ONE METHODS
+  //=========================================================================
+  @GetMapping("OneMethod")
+  String oneMethod(
+    @RequestParam String  name,
+    @RequestParam Integer age
+  ) {
+    return name + " is " + age + " years old";
+  }
+
+  //=========================================================================
+  // TWO METHODS
+  //=========================================================================
+  @RequestMapping(value="TwoMethods", method = { RequestMethod.GET, RequestMethod.POST })
+  String twoMethods(
+    @RequestParam String  name,
+    @RequestParam Integer age
+  ) {
+    return name + " is " + age + " years old";
+  }
+
+
 }
+
+
